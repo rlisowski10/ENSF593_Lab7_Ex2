e70c5376931d34f5dff7b8d24af0a2e2544bcd4b
rlisowski10 <ryan.lisowski@outlook.com>
2019-11-07 14:38:10 -0700

    GUI messed up

--- src/TicTacToeApp.java
+++ src/TicTacToeApp.java
@@ -1,13 +1,12 @@
 import view.*;
-//import controller.*;
-//import model.*;
+import controller.*;
+import model.*;
 
 public class TicTacToeApp {
 
   public static void main(String[] args) {
     GameView gameView = new GameView();
-    //StudentRecordManager studentRecordManager = new StudentRecordManager();
-    //StudentRecordController studentRecordController = new StudentRecordController(studentRecordView, messageBoxView,
-    //    insertNodeView, studentRecordManager);
+    Game game = new Game();
+    GameController gameController = new GameController(gameView, game);
   }
 }
--- src/view/GameView.java
+++ src/view/GameView.java
@@ -10,32 +10,46 @@ import java.awt.event.ActionListener;
 
 public class GameView {
 
+  private JFrame gameFrame;
   private JPanel gamePanel;
   private GridBagLayout gbLayout;
   private GridBagConstraints gbConstraints;
+  private JButton[][] gameSquare;
 
   public GameView() {
-    JFrame gameFrame = new JFrame("Tic-Tac-Toe");
+    
+    setupGameFrame();
+    setupGamePanel();
+
+    
+    gameFrame.getContentPane().add(gamePanel);
+
+    
+    
+
+    createBoardButtons(3, 3);
+    setupButtons();
+    setupOtherComponents();
+
+    gameFrame.pack();
+    gameFrame.setVisible(true);
+  }
+
+  private void setupGameFrame() {
+    gameFrame = new JFrame("Tic-Tac-Toe");
     gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     gameFrame.setLayout(new GridLayout());
+  }
 
+  private void setupGamePanel() {
     gamePanel = new JPanel();
     gbLayout = new GridBagLayout();
     gbConstraints = new GridBagConstraints();
-
     gbConstraints.insets = new Insets(1, 1, 1, 1);
     gamePanel.setLayout(gbLayout);
-    gameFrame.getContentPane().add(gamePanel);
+  }
 
-    JButton gameSquareButton1 = new JButton("1");
-    JButton gameSquareButton2 = new JButton("2");
-    JButton gameSquareButton3 = new JButton("3");
-    JButton gameSquareButton4 = new JButton("4");
-    JButton gameSquareButton5 = new JButton("5");
-    JButton gameSquareButton6 = new JButton("6");
-    JButton gameSquareButton7 = new JButton("7");
-    JButton gameSquareButton8 = new JButton("8");
-    JButton gameSquareButton9 = new JButton("9");
+  private void setupOtherComponents() {
     JButton setupGameButton = new JButton("Start");
     JLabel markerLabel = new JLabel("Marker:");
     JLabel playerLabel = new JLabel("Player:");
@@ -48,15 +62,6 @@ public class GameView {
     messageArea.setLineWrap(true);
     JScrollPane messageAreaScrollPane = new JScrollPane(messageArea);
 
-    addComponentsToPanel(gameSquareButton1, 0, 0, 30, 50, 1);
-    addComponentsToPanel(gameSquareButton2, 1, 0, 30, 50, 1);
-    addComponentsToPanel(gameSquareButton3, 2, 0, 30, 50, 1);
-    addComponentsToPanel(gameSquareButton4, 0, 1, 30, 50, 1);
-    addComponentsToPanel(gameSquareButton5, 1, 1, 30, 50, 1);
-    addComponentsToPanel(gameSquareButton6, 2, 1, 30, 50, 1);
-    addComponentsToPanel(gameSquareButton7, 0, 2, 30, 50, 1);
-    addComponentsToPanel(gameSquareButton8, 1, 2, 30, 50, 1);
-    addComponentsToPanel(gameSquareButton9, 2, 2, 30, 50, 1);
     addComponentsToPanel(setupGameButton, 2, 3, 9, 5, 2);
     addComponentsToPanel(markerLabel, 0, 3, 0, 0, 1);
     addComponentsToPanel(playerLabel, 0, 4, 0, 0, 1);
@@ -64,13 +69,23 @@ public class GameView {
     addComponentsToPanel(playerText, 1, 4, 30, 0, 1);
     addComponentsToPanel(messageLabel, 3, 0, 0, 0, 1);
     addComponentsToPanel(messageAreaScrollPane, 3, 1, 200, 132, 2);
+  }
 
-    gameFrame.pack();
-    gameFrame.setVisible(true);
+  private void setupButtons() {
+    addComponentsToPanel(gameSquare[0][0], 0, 0, 30, 50, 1);
+    addComponentsToPanel(gameSquare[0][1], 1, 0, 30, 50, 1);
+    addComponentsToPanel(gameSquare[0][2], 2, 0, 30, 50, 1);
+    addComponentsToPanel(gameSquare[1][0], 0, 1, 30, 50, 1);
+    addComponentsToPanel(gameSquare[1][1], 1, 1, 30, 50, 1);
+    addComponentsToPanel(gameSquare[2][2], 2, 1, 30, 50, 1);
+    addComponentsToPanel(gameSquare[2][0], 0, 2, 30, 50, 1);
+    addComponentsToPanel(gameSquare[2][1], 1, 2, 30, 50, 1);
+    addComponentsToPanel(gameSquare[2][2], 2, 2, 30, 50, 1);
   }
 
   public void addComponentsToPanel(Component component, int gridXPos, int gridYPos, int iPadX, int iPadY,
       int gridHeight) {
+    
     gbConstraints.gridx = gridXPos;
     gbConstraints.gridy = gridYPos;
 
@@ -81,5 +96,15 @@ public class GameView {
 
     gbLayout.setConstraints(component, gbConstraints);
     gamePanel.add(component);
+  }
+
+  public void createBoardButtons(int rows, int columns) {
+    gameSquare = new JButton[rows][columns];
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        gameSquare[i][j] = new JButton();
+      }
+    }
   }
 }
